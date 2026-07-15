a8c7823370ffa8ed49b70a2cf62f35bf3b92669c
Selvam Thirumal <selvamthirumal22@gmail.com>
2026-07-15 23:27:47 +0530

    first commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,9 +8,8 @@ const generateToken = (res, userId) => {
   // set JWT token as HTTP only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    // secure: true, // only works on https
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
